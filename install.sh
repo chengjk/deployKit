0352f5d4294fc4035ad96291078bcc9e8114f0fb
jacky <jacky.cheng@yeahmobi.com>
2017-07-20 18:20:15 +0800

    sh

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-if [ ! -f install ]; then
+if [ ! -f "install.sh" ]; then
 echo 'install must be run within its container folder' 1>&2
 exit 1
 fi
 
 CURDIR=`pwd`
 OLDGOPATH="$GOPATH"
+OLDGOBIN="$GOBIN"
 export GOPATH="$CURDIR"
+export GOBIN="$CURDIR"/bin
+
+echo $GOBIN
 
 gofmt -w src
 
-go install test
+go install src/ndp/dk/dk.go
 
 export GOPATH="$OLDGOPATH"
+export GOBIN="$OLDGOBIN"
 
 echo 'finished'
