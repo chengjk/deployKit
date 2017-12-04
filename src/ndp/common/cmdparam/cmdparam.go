d84862043064f517a47698258159c2c8d83cbc61
jacky <jacky.cheng@yeahmobi.com>
2017-12-04 18:51:21 +0800

    add prefix cmd

--- src/ndp/common/cmdparam/cmdparam.go
+++ src/ndp/common/cmdparam/cmdparam.go
@@ -10,10 +10,10 @@ import (
 
 func Parse() (model.CmdParam, error) {
 	name := flag.String("name", "config", "名称,可作为变量{name}使用,对应配置文件名.不能使用变量. e.g. ec表示使用ec.json.")
-	tag := flag.String("tag", "", "标签.可作为变量{tag}使用.不能使用变量. e.g. v1.0")
+	tag := flag.String("tag", "", "标签.一般是版本信息,可作为变量{tag}使用.不能使用变量. e.g. v1.0")
 	url := flag.String("url", "", "外网仓库地址.直接在服务器上 wget. e.g. http://test.com/{tag}/{name}.zip.")
 	localUrl := flag.String("lurl", "", "内网仓库地址,需要先下载到本地磁盘再上传服务器. e.g. http://127.0.0.1/{tag}/{name}.zip.")
-	zipPath := flag.String("path", "", "目标文件在本地磁盘路径. e.g. /tmp/{tag}/{name}.zip.")
+	zipPath := flag.String("path", "", "目标文件路径. e.g. /tmp/{tag}/{name}.zip.")
 	pcmd := flag.String("pcmd", "", "prefix cmd,文件上传前在server的workDir中执行，分号隔开.e.g. mkdir p")
 	scmd := flag.String("scmd", "", "suffix cmd,文件上传后在server的workDir中执行，分号隔开.e.g. rm -f *.zip")
 	v:=flag.Bool("v", false,"show current version.")
