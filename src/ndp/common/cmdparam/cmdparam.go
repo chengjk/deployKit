4b10786a6ba7293e955e62dd2e4882f3821fe00e
jacky <jacky.cheng@yeahmobi.com>
2017-12-04 18:47:17 +0800

    add prefix cmd

--- src/ndp/common/cmdparam/cmdparam.go
+++ src/ndp/common/cmdparam/cmdparam.go
@@ -9,7 +9,7 @@ import (
 )
 
 func Parse() (model.CmdParam, error) {
-	name := flag.String("name", "config", "名称可作为变量{name}使用,对应配置文件名.不能使用变量. e.g. ec表示使用ec.json.")
+	name := flag.String("name", "config", "名称,可作为变量{name}使用,对应配置文件名.不能使用变量. e.g. ec表示使用ec.json.")
 	tag := flag.String("tag", "", "标签.可作为变量{tag}使用.不能使用变量. e.g. v1.0")
 	url := flag.String("url", "", "外网仓库地址.直接在服务器上 wget. e.g. http://test.com/{tag}/{name}.zip.")
 	localUrl := flag.String("lurl", "", "内网仓库地址,需要先下载到本地磁盘再上传服务器. e.g. http://127.0.0.1/{tag}/{name}.zip.")
